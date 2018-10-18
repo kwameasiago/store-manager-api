fe9b74264e231e09863b36653795ac3f3f16f83a
SelaDanti <kwameasiago@gmail.com>
2018-10-18 13:59:07 +0300

    :recycle: [Chore #161286591] Code climate grade

--- app/api/v1/model/sales.py
+++ app/api/v1/model/sales.py
@@ -8,18 +8,20 @@ class Sales(Verification):
 		self.items = items
 
 	def check_sales_input(self):
-		if self.is_sales_payload(self.items) is False:
+		items = self.items
+		if self.is_sales_payload(items) is False:
 			return {'result': 'invalid payload'},406
-		elif self.items['quantity'] < 1:
+		elif items['quantity'] < 1:
 			return {'result': 'quantity can not be less than one'},406
-		elif self.items['productId'] < 0:
+		elif items['productId'] < 0:
 			return {'result': 'productId can not be less than zero'},406
 		else:
 			return 1
 	
 	def add_sales(self):
-		pid = Products.get_one(self.items['productId'])
-		total = pid[self.items['productId']]['price'] * self.items['quantity']
-		self.items['price'] = total
-		Sales.sales.append(self.items)
+		items = self.items
+		pid = Products.get_one(items['productId'])
+		total = pid[items['productId']]['price'] * items['quantity']
+		items['price'] = total
+		Sales.sales.append(items)
 		return {'result': 'sales added'},201
