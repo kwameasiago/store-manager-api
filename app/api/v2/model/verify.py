b9e6563d9a48427091532a11353172fdc0f403f6
SelaDanti <kwameasiago@gmail.com>
2018-10-23 17:15:28 +0300

    :new: [Feature #161413844] Made test to pass

--- app/api/v2/model/verify.py
+++ app/api/v2/model/verify.py
@@ -5,19 +5,40 @@ class Verify:
 	class to verify inputs
 	"""
 	def is_empty(self,items):
-		pass
+		for item in items:
+			if bool(item) is False:
+				return {'error': 'empty data set detected'}
+		return  1
 
 	def is_whitespace(self,items):
-		pass
+		for item in items:
+			if item.isspace() is True:
+				return {'error': 'whitespace data detected'}
+		return 1
 
-	def is_payload(self,items,lists):
-		pass
 
-	def is_email(self,items):
-		pass
+	def is_payload(self,items,keys):
+		items = items.keys()
+		if len(items) == len(keys):
+			for item in items:
+				if item not in keys:
+					return {'error': 'invalid payload'}
+			return 1
+		else:
+			return {'error': 'invalid payload'}
+
+	def is_email(self,email):
+		result = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+		if result is None:
+			res = {'error': 'invalid email detected'}
+		else:
+			res = 1
+		return res
 
 	def less_than(self,num1,num2):
-		pass
+		if num1 < num2:
+			res = True
+		else:
+			res = False
+		return res
 
-	def is_email(self,items):
-		pass
--- test/v2/test_validators.py
+++ test/v2/test_validators.py
@@ -16,24 +16,44 @@ class TestValidators(unittest.TestCase):
 	# test if data is empty
 	def test_empty_data(self):
 		test = self.obj.is_empty(['','d'])
+		self.assertEqual(test,{'error': 'empty data set detected'})
+
+	# test if data is not empty
+	def test_not_empty_data(self):
+		test = self.obj.is_empty(['d','d'])
 		self.assertEqual(test,1)
 
-	# test if whitespace exists
+	# test if whitespace does not  exists
 	def test_is_whitespace(self):
 		test = self.obj.is_whitespace(['  ','d'])
+		self.assertEqual(test,{'error': 'whitespace data detected'})
+
+	# test if whitespace exists
+	def test_is_not_whitespace(self):
+		test = self.obj.is_whitespace(['s','d'])
 		self.assertEqual(test,1)
 
 	# test if item is not a playload
 	def test_is_not_payload(self):
 		test = self.obj.is_payload({'name':'dad','age':9},['name','age','class'])
+		self.assertEqual(test,{'error': 'invalid payload'})
+
+	# test if item is a playload
+	def test_is_not_payload(self):
+		test = self.obj.is_payload({'name':'dad','age':9, 'class':3},['name','age','class'])
 		self.assertEqual(test,1)
 
 	# test if item is not an email
 	def test_is_not_email(self):
 		test = self.obj.is_email('notmail.com')
-		self.assertEqual(test,1)
+		self.assertEqual(test, {'error': 'invalid email detected'})
+
+	# test if item is an email
+	def test_is_email(self):
+		test = self.obj.is_email('mail@gmail.com')
+		self.assertEqual(test, 1)
 
 	# test if num1 is less than num 2
 	def test_less_than(self):
-		test = self.obj.less_than(2,1)
-		self.assertEqual(test,1)
+		test = self.obj.less_than(2,11)
+		self.assertTrue(test)
